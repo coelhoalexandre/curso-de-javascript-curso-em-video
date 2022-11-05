4d02b992ae94be8b304334399bcebcc8f97c065f
Alexandre Coelho <116454874+alexandrecoelhowebdev@users.noreply.github.com>
2022-11-05 11:18:52 -0300

    aula014

---
+++ aula013-014/ambiente.js
@@ -0,0 +1,27 @@
+//AULA 013
+
+console.log('Com o while')
+
+var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c} `)
+    c++
+}
+
+console.log('Com o do while')
+
+c = 1
+
+do {
+    console.log(`Passo ${c} `)
+    c++
+} while (c <= 5)
+
+
+//AULA 014
+
+console.log('Com o for')
+
+for (var c = 1; c <= 5; c++) {
+    console.log(`Passo ${c}`)
+}
